2bc697a12dbe2ae290c9e6a3e71e82c6f2d61ddb
deborah <deborahadebiyi@hotmail.co.uk>
2020-11-22 15:20:16 +0000

    rectified src and spec file contents

--- src/office.js
+++ src/office.js
@@ -1,17 +1,15 @@
-describe("Office", function(){
-  var office;
-  beforeEach(function(){
-    office = new Office();
-  });
-  it("checks if a meeting room is available", function(){
-    expect(office.available).toBe(true);
-  });
-  it("meeting room becomes unavailable when occupied", function(){
-    office.occupyRoom();
-    expect(office.available).toBe(false);
-  });
-  it("meeting room becomes available when vacant", function(){
-    office.vacateRoom();
-    expect(office.available).toBe(true);
-  });
-})
+class Office {
+
+  constructor(){
+    this.available = true;
+  };
+
+  occupyRoom(){
+    this.available = false;
+  };
+
+  vacateRoom(){
+    this.available = true;
+  }
+
+}
